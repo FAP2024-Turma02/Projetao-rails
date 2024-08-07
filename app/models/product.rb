416335b902299810ed240b890819081f0cae82d6
Carlosguimara <carlos.henrique.ccruz@gmail.com>
2024-08-07 17:48:13 -0300

    Alteração Tabela Users, campo document (unique) e Desafio extra, criação da tabela Products com varias validações

---
+++ app/models/product.rb
@@ -0,0 +1,23 @@
+class Product < ApplicationRecord
+    # Validações para o campo name
+    validates :name, presence: true, length: { minimum: 3, maximum: 100 }
+  
+    # Validações para o campo SKU
+    validates :sku, presence: true, uniqueness: true
+  
+    # Validação para o campo price
+    validates :price, numericality: { greater_than: 0 }
+  
+    # Validação para o campo stock_quantity
+    validates :stock_quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+
+    # Callback para arredondar o preço
+    before_save :round_price
+
+    private
+
+    def round_price
+        self.price = price.round(2) if price.present?
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-    validates_presence_of :name, :document
+    validates_presence_of :name
+    validates :document, presence: true, uniqueness: true, length: { in: 6..12, message: "must be between 6 and 12 characters" }
     enum :role, [:intern, :employee, :manager ]
 end
+
+  
